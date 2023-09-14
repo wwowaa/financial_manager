15472641ee47b0c4be257b590e361a5acdb4499f
wwowaa <vovanovak2209@gmail.com>
2023-09-14 20:21:03 +0300

    First Commit

---
+++ finmanager/finmanager/src/main/java/com/example/finmanager/FinmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.finmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmanagerApplication.class, args);
+	}
+
+}
---
+++ finmanager/finmanager/src/test/java/com/example/finmanager/FinmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
